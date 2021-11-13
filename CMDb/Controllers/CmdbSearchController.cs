71a8196b20f6ea03939010a8bff84757f611bf77
JonasHagstrom79 <73818160+JonasHagstrom79@users.noreply.github.com>
2021-11-14 00:22:40 +0100

    Add project files.

---
+++ CMDb/Controllers/CmdbSearchController.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CMDb.Models;
+using CMDb.Models.ViewModels;
+using CMDb.Repositories;
+using Microsoft.Extensions.Logging;
+using System.Diagnostics;
+
+namespace CMDb.Controllers
+{
+    public class CmdbSearchController : Controller
+    {
+        List<TopMoviePropertiesOmdbDto> movies = new List<TopMoviePropertiesOmdbDto>();
+        private readonly IRepository repository;
+
+        public CmdbSearchController(IRepository repository)
+        {
+            this.repository = repository;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> CmdbSearchResult(string searchInput)//Söker efter filmer i Cineasternas databas
+        {
+            try
+            {
+                var tasks = new List<Task>();
+                var topmovies = await repository.GetTopMoviesAsync();//Hämtar en lista från Cineasternas api
+                foreach (var topmovie in topmovies)
+                {
+                    tasks.Add(
+                        Task.Run(
+                            async () =>
+                            {
+                                var movie = await repository.GetMovie(topmovie.ImdbID);//Hämtar topfilmen från Omdb
+                                if (movie.Title != null && movie.ImdbID != null)//För att undvika att felaktiga filmer läggs till i listan
+                                {
+                                    movies.Add(movie);
+                                }
+                            }
+                        ));
+
+                }
+                await Task.WhenAll(tasks);//Klar
+
+                var model = new CmdbSearchViewModel(movies, searchInput);
+                return View("Index", model);//Skickar informationen
+            }
+            catch (System.Exception)//För felhantering
+            {
+                var model = new CmdbSearchViewModel();
+                ModelState.AddModelError(string.Empty, "No contact with Api");
+                return View(model);
+                throw;
+
+            }
+
+        }
+    }
+}
---
+++ CMDb/Models/ViewModels/CmdbSearchViewModel.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CMDb.Models.ViewModels
+{
+    public class CmdbSearchViewModel: MovieRatingsOmdbDto
+    {
+        #region Object
+        public TopMoviePropertiesOmdbDto FakeMovie = new TopMoviePropertiesOmdbDto();
+        #endregion
+
+        #region Constructors
+        public CmdbSearchViewModel(List<TopMoviePropertiesOmdbDto> topmoviesomdb, String searchinput)
+        {
+            //Sorterar sökresultatet
+            this.TopMovieList = topmoviesomdb.Where
+            (x => x.Title.Contains(searchinput) ||
+            x.Title.Contains(searchinput.ToUpper()) ||
+            x.Title.Contains(searchinput.ToLower()) ||
+            x.Title.Contains(char.ToUpper(searchinput[0]).ToString() + (searchinput.Substring(1))) ||
+            x.Title.Contains(char.ToLower(searchinput[0]).ToString() + (searchinput.Substring(1)))).ToList();
+            IsListNull();
+        }
+        public CmdbSearchViewModel()//För att hantera fel i controllern
+        {
+
+        } 
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Skapar ett objekt om lita är tom
+        /// </summary>
+        private void IsListNull()
+        {
+            if (TopMovieList.Count == 0)
+            {
+                FakeMovie.ImdbID = "";
+                FakeMovie.Title = "";
+                FakeMovie.Poster = "";
+                TopMovieList.Add(FakeMovie);
+            }
+        } 
+        #endregion
+
+    }
+}
